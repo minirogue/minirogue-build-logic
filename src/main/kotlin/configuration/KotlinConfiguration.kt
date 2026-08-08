e9396f1fa607b4e5b8a3b78bd4f7d96a67230e48
shwitte <ettiwsl@gmail.com>
2026-08-08 13:47:32 -0700

    Use JVM toolchain

--- src/main/kotlin/configuration/KotlinConfiguration.kt
+++ src/main/kotlin/configuration/KotlinConfiguration.kt
@@ -1,20 +1,12 @@
 package configuration
 
-import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+import org.gradle.jvm.toolchain.JavaLanguageVersion
 import versions.JAVA_VERSION
 
 public fun Project.configureJvm() {
     extensions.configure(JavaPluginExtension::class.java) {
-        sourceCompatibility = JavaVersion.toVersion(JAVA_VERSION)
-        targetCompatibility = JavaVersion.toVersion(JAVA_VERSION)
-    }
-    tasks.withType(KotlinJvmCompile::class.java).configureEach {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.fromTarget(JAVA_VERSION))
-        }
+        toolchain { languageVersion.set(JavaLanguageVersion.of(JAVA_VERSION)) }
     }
 }
